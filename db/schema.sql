0b7ac42041a257f02b4419f060c6e15ce2103a0b
Corvus <wyatta.hancock@gmail.com>
2020-08-17 10:53:04 -0600

    added the necessecary empty files

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE IF NOT EXISTS burgers_db;
+USE burgers_db;
+
+DROP TABLE IF EXISTS burgers;
+
+-- The table that will contain all of the burgers that the user decides upon
+CREATE TABLE burgers (
+    id int NOT NULL AUTO_INCREMENT,
+    burger_name varchar(255) NOT NULL,
+    devoured BOOLEAN DEFAULT false,
+    PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+USE burgers_db;
+INSERT INTO burgers (burger_name) VALUES ('Jalapeno Burger');
+INSERT INTO burgers (burger_name) VALUES ('Bacon Burger with Guacamole');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Gorgonzola Burger with carmalized onions');
