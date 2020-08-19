6f986a880a87673645c72189501f9e59373e5799
Corvus <wyatta.hancock@gmail.com>
2020-08-19 09:51:19 -0600

    added not null to schema

--- db/schema.sql
+++ db/schema.sql
@@ -7,6 +7,6 @@ DROP TABLE IF EXISTS burgers;
 CREATE TABLE burgers (
     id int NOT NULL AUTO_INCREMENT,
     burger_name varchar(255) NOT NULL,
-    devoured BOOLEAN DEFAULT false,
+    devoured BOOLEAN NOT NULL DEFAULT false,
     PRIMARY KEY (id)
 );
